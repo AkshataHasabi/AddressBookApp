fcb6f817b91c39fce60bc2c91708fe92643eb963
AkshataHasabi <akshuh818@gmail.com>
2022-05-25 22:40:28 +0530

    ADDED:welcome message

--- src/main/java/com/example/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/example/addressbookapp/AddressBookAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AddressBookAppApplication {
 
     public static void main(String[] args) {
+        System.out.println("welcome to AddressBookApp development");
         SpringApplication.run(AddressBookAppApplication.class, args);
     }
 
