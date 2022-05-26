f948d69a03243be8e7726e3f300a25f4f3a9b893
AkshataHasabi <akshuh818@gmail.com>
2022-05-26 14:36:32 +0530

    ADDED:ability to add validation to name fields so rest call can be validated

--- src/main/java/com/example/addressbookapp/dto/AddressbookDTO.java
+++ src/main/java/com/example/addressbookapp/dto/AddressbookDTO.java
@@ -2,14 +2,25 @@ package com.example.addressbookapp.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 @Data
 public class AddressbookDTO {
-    private String fName;
-    private String lName;
-    private String phoneNumber;
-    private String email;
-    private String address;
-    private String city;
-    private String state;
-    private int zipCode;
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message ="Addressbook author first name pattern is invalid")
+    public String fName;
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message ="Addressbook author last name pattern is invalid")
+    public String lName;
+    @NotNull(message = "phone number should not be empty")
+    public String phoneNumber;
+    @Pattern(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", message="Email pattern is invalid")
+    public String email;
+    @NotBlank(message = "address book should not be empty")
+    public String address;
+    @NotBlank(message = "city should not be empty")
+    public String city;
+    public String state;
+    //@Pattern(regexp = "^[0-9]{5}(-[0-9]{4}?)$",message = "zipcode is invalid")
+    public int zipCode;
 }
