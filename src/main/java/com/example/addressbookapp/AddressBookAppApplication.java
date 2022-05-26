c496233c12c066f83f5e64636e7933c69b61f675
AkshataHasabi <akshuh818@gmail.com>
2022-05-26 09:14:50 +0530

    ADDED:ability create application properties for dev and production

--- src/main/java/com/example/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/example/addressbookapp/AddressBookAppApplication.java
@@ -1,14 +1,16 @@
 package com.example.addressbookapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookAppApplication {
-
     public static void main(String[] args) {
         System.out.println("welcome to AddressBookApp development");
-        SpringApplication.run(AddressBookAppApplication.class, args);
+        ApplicationContext context=SpringApplication.run(AddressBookAppApplication.class, args);
+        log.info("Employee payrollapp started {} environment",context.getEnvironment().getProperty("environment"));
     }
-
 }
